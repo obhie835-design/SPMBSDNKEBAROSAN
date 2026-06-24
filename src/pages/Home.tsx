fbe06ea6d9658c76767aa9edb9d4ca57e7376936
obhie835-design <obhie835@gmail.com>
2026-06-24 10:57:53 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,9 +57,9 @@ export default function Home() {
               transition={{ duration: 0.6, delay: 0.1 }}
               className="text-5xl md:text-6xl font-extrabold text-slate-900 tracking-tight leading-tight mb-6"
             >
-              H.E.B.A.T <br className="hidden md:block" />
+              C.G.I <br className="hidden md:block" />
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-500">
-                Hidup dengan Etika, Berakhlak dan Berprestasi
+                Cemerlang, Gemilang dan Inovatif
               </span>
             </motion.h1>
             
